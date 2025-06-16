aaf28114577f7598d41b181865844b1e250a9b09
Mathx8 <matheusqueirozdev@gmail.com>
2025-06-16 14:39:29 -0300

    teste rotas

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ app = Flask(__name__)
 CORS(app, 
      resources={
          r"/*": {
-             "origins": ["http://localhost:3000", "https://localhost:3000"],
+             "origins": ["http://localhost:3000"],
              "methods": ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
              "allow_headers": ["Content-Type", "Authorization"],
              "supports_credentials": True,
@@ -41,12 +41,12 @@ db.init_app(app)
 jwt = JWTManager(app)
 
 configure_swagger(app)
-app.register_blueprint(Jogador_Blueprint, url_prefix="/jogador")
+'''app.register_blueprint(Jogador_Blueprint, url_prefix="/jogador")
 app.register_blueprint(Partida_Blueprint, url_prefix="/partida")
 app.register_blueprint(Time_Blueprint, url_prefix="/time")
 app.register_blueprint(Competicao_Blueprint, url_prefix="/competicao")
 app.register_blueprint(Grupo_Blueprint, url_prefix="/grupo")
-app.register_blueprint(Backup_Blueprint, url_prefix="/backup")
+app.register_blueprint(Backup_Blueprint, url_prefix="/backup")'''
 
 with app.app_context():
     db.create_all()
